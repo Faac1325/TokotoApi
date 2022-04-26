dfa0902637aac999aef3387c743f85ef0701984d
Fabian Arevalo <ingfabianarevalo_13@gmail.com>
2022-04-25 22:28:05 -0500

    IMPLEMENTACION - Slider
    * Se agrega es slider a vista  splash screen

--- app/src/main/java/fabian/arevalo/tokotoapi/Splash_screen.java
+++ app/src/main/java/fabian/arevalo/tokotoapi/Splash_screen.java
@@ -1,25 +1,77 @@
 package fabian.arevalo.tokotoapi;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.window.SplashScreen;
 
+import com.smarteist.autoimageslider.IndicatorView.animation.type.IndicatorAnimationType;
+import com.smarteist.autoimageslider.SliderAnimations;
+import com.smarteist.autoimageslider.SliderView;
+
+import java.util.List;
+
 public class Splash_screen extends AppCompatActivity {
+     Button btnsplash;
+     TextView tvtokoto,tvsplash;
+
+    private RecyclerView recyclerView;
+    private int[] images;
+    private String[] text;
+    private SliderAdapter adapter;
+    private SliderView sliderView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
 
-        //de aca empiza el splash
-        new Handler().postDelayed(() -> {
-            Intent intent= new Intent(getApplicationContext(),Registro.class);
-            startActivity(intent);
-            finish();
-        },2000);
+        btnsplash=findViewById(R.id.btnsplash);
+        tvtokoto=findViewById(R.id.btnsplash);
+        tvsplash=findViewById(R.id.tvsplash);
+
+
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        linearLayoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
+        sliderView = findViewById(R.id.sliderView);
+        images = new int[]{R.drawable.splash_1, R.drawable.splash_2, R.drawable.splash_3};
+        text = new String[]{"", "", ""};
+
+        adapter = new SliderAdapter(images, text);
+        sliderView.setSliderAdapter(adapter);
+
+        sliderView.setSliderTransformAnimation(SliderAnimations.SIMPLETRANSFORMATION);
+        sliderView.setIndicatorAnimation(IndicatorAnimationType.DROP);
+        sliderView.startAutoCycle();
+
+
+        //Este nos envia de Splash a MainActivity
+        btnsplash.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(Splash_screen.this, MainActivity.class);
+                startActivity(i);
+                onBackPressed();
+            }
+        });
+
 
     }
+    //Bloquear atras
+    @Override
+    public void onBackPressed() {
+        /*super.onBackPressed();*/
+    }
+
+
 }
